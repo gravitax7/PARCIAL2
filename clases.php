dbc348b0ed1eab68b3170d683766c2f33cf65895
Diana orozco <marthin_0712@hotmail.com>
2024-09-26 20:09:17 -0500

    6. Modificacion de la clase GestorBiblioteca para usar las clases hijas, Alvan Samudio

--- clases.php
+++ clases.php
@@ -13,9 +13,7 @@ abstract class RecursoBiblioteca implements prestable{
     public $fechaAdquisicion;
     public $tipo;
 
-    public function obtenerDetallesPrestamo (){
-        
-    }
+
     public function __construct($datos) {
         foreach ($datos as $key => $value) {
             if (property_exists($this, $key)) {
@@ -68,7 +66,7 @@ class DVD extends RecursoBiblioteca implements prestable{
     }
 }
 
-class GestorBiblioteca  {
+class GestorBiblioteca extends RecursoBiblioteca {
     private $recursos = [];
 
     public function cargarRecursos() {
@@ -76,9 +74,21 @@ class GestorBiblioteca  {
         $data = json_decode($json, true);
         
         foreach ($data as $recursoData) {
-            $recurso = new RecursoBiblioteca($recursoData);
-            $this->recursos[] = $recurso;
-        }
+            foreach ($data as $recursoData) {
+                switch ($recursoData['tipo']) {
+                    case 'Libro':
+                        $recurso = new Libro($recursoData, $recursoData['isbn']);
+                        break;
+                    case 'Revista':
+                        $recurso = new Revista($recursoData, $recursoData['numeroEdicion']);
+                        break;
+                    case 'DVD':
+                        $recurso = new DVD($recursoData, $recursoData['duracion']);
+                        break;
+                    default:
+                        continue;
+                }
+                $this->recursos[] = $recurso;
         
         return $this->recursos;
     }
